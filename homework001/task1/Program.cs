d2a7dedd9ab88cc6fb84426fc0b8c1544d7ebaff
Alex Kopylov <kopylov.alex.evg@gmail.com>
2022-04-14 23:44:40 +0300

    add task 1

---
+++ homework001/task1/Program.cs
@@ -0,0 +1,5 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+Console.Write("Введите 3хзначное число ");
+int number=Convert.ToInt32(Console.ReadLine());
+int result = number % 10;
+Console.Write(result);
