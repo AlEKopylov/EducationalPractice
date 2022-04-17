2721b7c4b5debc4788b11f9a71c5507829048942
Alex Kopylov <kopylov.alex.evg@gmail.com>
2022-04-18 00:43:47 +0300

    add task 3

---
+++ homework002/task3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Write("Введите число -> ");
+int number = Convert.ToInt32(Console.ReadLine());
+Compare(number);
+
+void Compare(int numberIn)
+    {
+        if (numberIn == 6 ^ numberIn == 7)
+        { 
+            Console.WriteLine("Да");
+        }
+        else
+        {
+            Console.WriteLine("Нет");
+        }
+    }
