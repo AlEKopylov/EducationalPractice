4ad10ca0a4e472adb29a3fa1b082bfc8561adf10
Alex Kopylov <kopylov.alex.evg@gmail.com>
2022-04-18 00:27:51 +0300

    add task 2

---
+++ homework002/task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число -> ");
+string number = Console.ReadLine();
+
+if (number.Length >= 3)
+{
+    Console.WriteLine(RezakNumber(number));
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
+
+char RezakNumber(string numberToRez)
+    {
+        char result = numberToRez[2];
+        return result;
+    }
