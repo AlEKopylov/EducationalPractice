3c21de47f54d1826a618565333dad86d30bdab58
Alex Kopylov <kopylov.alex.evg@gmail.com>
2022-04-18 00:16:06 +0300

    add task 1

---
+++ homework002/task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите трехзначное число -> ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(RezakNumber(number));
+
+int RezakNumber(int numberToRez)
+    {
+        int result = (numberToRez / 10)%10;
+        return result;
+    }
