c4ce0505320c9fb047c34bcf9e049e8a72ebc4e4
Alex Kopylov <kopylov.alex.evg@gmail.com>
2022-04-19 00:33:57 +0300

    add task 3

---
+++ homework003/task3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+perebor(number);
+
+void perebor(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.Write(UpToKub(count) + " ");
+        count++;
+    }
+}
+
+double UpToKub(int vhod)
+{
+    return Math.Pow(vhod, 3);
+}
+
