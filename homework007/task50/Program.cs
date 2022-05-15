9de6ead226a07ee678b4efb2937f2c4fbc7dd3bf
Alex Kopylov <kopylov.alex.evg@gmail.com>
2022-05-16 01:30:38 +0300

    add task 50

---
+++ homework007/task50/Program.cs
@@ -0,0 +1,44 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такой элемент отсутствует.
+
+// создание и заполнение массива случайными числами
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int collums = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, collums];
+FillArray(rows, collums, array);
+WriteArray(rows, collums, array);
+
+//поиск элемента
+Console.Write($"Введите номер строки: ");
+int findRows = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Введите номер столбца: ");
+int findCollum = Convert.ToInt32(Console.ReadLine());
+if (findCollum <= array.GetUpperBound(1)+1
+    & findRows <= array.GetUpperBound(0)+1)
+{
+    Console.Write($"Ваше число на позиции {findRows}{findCollum} -> {array[findRows - 1, findCollum - 1]}");
+}
+else Console.Write($"{findRows}{findCollum}-> такого числа в массиве нет"); ;
+///////////////////////////////////////////////
+void FillArray(int m, int n, int[,] array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().Next(-10, 10); // получаем генерацию случайных вещественных чисел от -10 до 10
+        }
+    }
+}
+void WriteArray(int m, int n, int[,] array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
