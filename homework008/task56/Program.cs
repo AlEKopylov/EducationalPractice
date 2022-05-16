d6d85f148b08f76d77febc5bdb6018b9acf1e886
Alex Kopylov <kopylov.alex.evg@gmail.com>
2022-05-17 02:20:39 +0300

    add task 56

---
+++ homework008/task56/Program.cs
@@ -0,0 +1,63 @@
+﻿//  Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+int rows = Input("Введите количество строк в массиве - ");
+int columns = Input("Введите количество столбцов в массиве - ");
+int[,] arrayOfNumbers = new int[rows, columns];
+
+FillArray(rows, columns, arrayOfNumbers);
+WriteArray(rows, columns, arrayOfNumbers);
+int[] rowWithMinimalSum = FinRowWithMinSum(arrayOfNumbers);
+Console.WriteLine("");
+Console.Write($"Минимальная сумма {rowWithMinimalSum[0]} найдена в строке {rowWithMinimalSum[1]+1}");
+
+int[] FinRowWithMinSum(int[,] array)
+{
+    int[] result = new int[2];
+    result[0] = 0;
+    result[1] = 0;
+    int tempSum = 0;
+    for (int i = 0; i < array.GetUpperBound(0) + 1; i++)
+    {
+        for (int j = 0; j < array.GetUpperBound(1) + 1; j++)
+        {
+            tempSum += array[i, j];
+        }
+        if (tempSum < result[0] | i == 0)
+        {
+            result[0] = tempSum;
+            result[1] = i;
+            tempSum = 0;
+        }
+        tempSum = 0;
+    }
+    return result;
+}
+
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillArray(int rows, int columns, int[,] array)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void WriteArray(int rows, int columns, int[,] array)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
