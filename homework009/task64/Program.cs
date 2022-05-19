d40013d8ed61c21b9dcf6e5075d5737c67739ba1
Alex Kopylov <kopylov.alex.evg@gmail.com>
2022-05-19 23:31:14 +0300

    add task 64

---
+++ homework009/task64/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+int N = Input("Введите N - ");
+NaturalOutput(N);
+
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int NaturalOutput(int number)
+{
+    Console.Write($"{number} ");
+    if (number > 1) return NaturalOutput(number-1);
+    return 1;
+}
